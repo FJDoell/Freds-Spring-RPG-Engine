c323fd77b05c790c7cc903461d1b3c0705a9462a
Fred-Doell <fjdoell@gmail.com>
2022-06-17 13:43:57 -0400

    Implemented getType into CharacterService

--- Workspace/Freds-Spring-RPG-Engine/src/main/java/com/character/CharacterService.java
+++ Workspace/Freds-Spring-RPG-Engine/src/main/java/com/character/CharacterService.java
@@ -3,6 +3,7 @@ package com.character;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.character.actor.Actor;
 import com.character.mods.equipment.EquipService;
 import com.character.mods.equipment.Equipment;
 
@@ -17,6 +18,22 @@ public class CharacterService {
 	
 	@Autowired
 	EquipService equipServ;
+	
+	/**
+	 * Get the type of this character: Actor, or Monster?<br>
+	 * True - Actor.<br>
+	 * False - Monster.
+	 * @param c
+	 * @return
+	 */
+	public boolean getType(CharacterModel c) {
+		if(Actor.class.isAssignableFrom(c.getClass())) {
+			System.out.println("This is an actor");
+			return true;
+		}
+		System.out.println("This is a monster");
+		return false;
+	}
 	
 	// Add equipment
 	public CharacterModel addEquip(CharacterModel c, Equipment equip) {
